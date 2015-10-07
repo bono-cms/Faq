059b077d9eb580a15bdc86bf2ae67fcf3bc60237
David Yang <daworld.ny@gmail.com>
2015-10-08 04:12:41 +0500

    Migrated Faq to standalone repository

---
+++ Config/module.config.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+return array(
+	'module' => array(
+		'name'	=> 'Faq',
+		'url'	=> '/admin/module/faq',
+		'icon'	=> '/module/Faq/Assets/faq.png',
+		'order' => 1,
+		'description' => 'FAQ module allows you to manage set of FAQs on your site',
+	)
+);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Faq
-Faq module
+FAQ module
+==========
+
+This module allows you to manage Frequently-Asked-Questions on your site. For details, please refer its documentation.
