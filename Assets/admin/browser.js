4a35fbfce9126f335ad81d0ded2fc4140fe26f76
David Yang <daworld.ny@gmail.com>
2015-10-30 07:25:50 +0500

    Removed boilerplate code

--- Assets/admin/browser.js
+++ Assets/admin/browser.js
@@ -1,6 +1,4 @@
-
-$(function() {
-	
+$(function(){
 	$.delete({
 		categories : {
 			main : {
@@ -8,42 +6,4 @@ $(function() {
 			}
 		}
 	});
-	
-	
-	$("[data-button='remove-selected']").click(function(event) {
-		event.preventDefault();
-		var data = $("form").serialize();
-		
-		$.ajax({
-			url		: "/admin/module/faq/delete-selected.ajax",
-			data	: data,
-			success : function(response) {
-				if (response == "1") {
-					window.location.reload();
-				} else {
-					$.showErrors(response);
-				}
-			}
-		});
-	});
-	
-	
-	$("[data-button='save-changes']").click(function(event) {
-		event.preventDefault();
-		var data = $("form").serialize();
-		
-		$.ajax({
-			url		: "/admin/module/faq/save.ajax",
-			data	: data,
-			success : function(response) {
-				if (response != "1") {
-					$.showErrors(response);
-				} else {
-					window.location.reload();
-				}
-			}
-		});
-	});
-	
-	
 });
