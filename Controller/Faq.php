1052853952e97afdb056bb687ab6a2be880d1659
David Yang <daworld.ny@gmail.com>
2016-07-31 17:57:03 +0500

    Fixed var name

--- Controller/Faq.php
+++ Controller/Faq.php
@@ -31,7 +31,7 @@ final class Faq extends AbstractController
             $faqManager = $this->getModuleService('faqManager');
 
             // Append breadcrumbs no
-            $this->view->getBreadcrumbBag()->addOne($faq->getTitle());
+            $this->view->getBreadcrumbBag()->addOne($page->getTitle());
 
             return $this->view->render('faq', array(
                 'faqs' => $faqManager->fetchAllPublished(),
