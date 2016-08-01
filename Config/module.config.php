adb9661c80168cb6baea89de5a44a6d303b3d4fa
David Yang <daworld.ny@gmail.com>
2016-08-01 22:28:00 +0500

    Removed extras from module configuration file

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,20 +1,9 @@
 <?php
 
-/**
- * This file is part of the Bono CMS
- * 
- * Copyright (c) No Global State Lab
- * 
- * For the full copyright and license information, please view
- * the license file that was distributed with this source code.
- */
-
 return array(
-    'module' => array(
-        'name'  => 'Faq',
-        'route' => 'Faq:Admin:Faq@gridAction',
-        'icon'  => 'fa fa-life-ring fa-5x',
-        'order' => 1,
-        'description' => 'FAQ module allows you to manage set of FAQs on your site',
-    )
+ 'name'  => 'Faq',
+ 'route' => 'Faq:Admin:Faq@gridAction',
+ 'icon'  => 'fa fa-life-ring fa-5x',
+ 'order' => 1,
+ 'description' => 'FAQ module allows you to manage set of FAQs on your site',
 );
