2de97f9518953234e9cd9aa9f9430a8d6e134a2b
David Yang <daworld.ny@gmail.com>
2017-05-15 18:30:51 +0500

    Longtext instead of text

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE `bono_module_faq` (
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`lang_id` INT NOT NULL COMMENT 'Language identification',
 	`question` TEXT	NOT NULL COMMENT 'Question text',
-	`answer` TEXT NOT NULL COMMENT 'Answer text',
+	`answer` LONGTEXT NOT NULL COMMENT 'Answer text',
 	`order` INT NOT NULL COMMENT 'Sort order',
 	`published` varchar(1) NOT NULL COMMENT 'Whether is published or not'
 	
