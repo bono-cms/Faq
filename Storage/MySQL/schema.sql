bd9a1ab7771ebfcbab167ef4e50ebed359aaef3b
David Yang <daworld.ny@gmail.com>
2017-08-21 18:50:16 +0500

    Added table for translations

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -1,14 +1,21 @@
 
 DROP TABLE IF EXISTS `bono_module_faq`;
 CREATE TABLE `bono_module_faq` (
-	
+
 	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`lang_id` INT NOT NULL COMMENT 'Language identification',
-	`question` TEXT	NOT NULL COMMENT 'Question text',
-	`answer` LONGTEXT NOT NULL COMMENT 'Answer text',
 	`order` INT NOT NULL COMMENT 'Sort order',
 	`published` varchar(1) NOT NULL COMMENT 'Whether is published or not'
-	
+
+) DEFAULT CHARSET = UTF8;
+
+DROP TABLE IF EXISTS `bono_module_faq_translations`;
+CREATE TABLE `bono_module_faq_translations` (
+
+	`id` INT NOT NULL,
+	`lang_id` INT NOT NULL COMMENT 'Language identification',
+	`question` TEXT	NOT NULL COMMENT 'Question text',
+	`answer` LONGTEXT NOT NULL COMMENT 'Answer text'
+
 ) DEFAULT CHARSET = UTF8;
 
 
