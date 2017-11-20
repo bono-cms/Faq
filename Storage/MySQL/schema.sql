3a51a482876c7ecb8ae31c61cf8f12a20b087935
David Yang <daworld.ny@gmail.com>
2017-11-20 23:18:04 +0500

    Added category_id column

--- Storage/MySQL/schema.sql
+++ Storage/MySQL/schema.sql
@@ -2,9 +2,10 @@
 DROP TABLE IF EXISTS `bono_module_faq`;
 CREATE TABLE `bono_module_faq` (
 
-	`id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`order` INT NOT NULL COMMENT 'Sort order',
-	`published` varchar(1) NOT NULL COMMENT 'Whether is published or not'
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `category_id` INT NOT NULL COMMENT 'Attached Category ID',
+    `order` INT NOT NULL COMMENT 'Sort order',
+    `published` varchar(1) NOT NULL COMMENT 'Whether is published or not'
 
 ) DEFAULT CHARSET = UTF8;
 
