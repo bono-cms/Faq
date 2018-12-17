213b62d63fcf72067f399a20ef7fbf6ab7334e72
dave-yang <daworld.ny@gmail.com>
2018-12-17 19:16:21 +0500

    Minor fixes. Use shared action partial

--- Config/module.config.php
+++ Config/module.config.php
@@ -4,7 +4,6 @@ return array(
  'name'  => 'Faq',
  'caption'  => 'Faq',
  'route' => 'Faq:Admin:Faq@gridAction',
- 'icon'  => 'fa fa-life-ring fa-5x',
  'order' => 1,
  'description' => 'FAQ module allows you to manage set of FAQs on your site',
 );
