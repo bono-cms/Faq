afb7300d1eeca435e116356b9057de19c268c03c
dave-yang <daworld.ny@gmail.com>
2018-12-19 14:35:12 +0500

    Use Grid component to generate table.

--- Controller/Admin/Faq.php
+++ Controller/Admin/Faq.php
@@ -161,8 +161,8 @@ final class Faq extends AbstractController
         $service = $this->getModuleService('faqManager');
 
         // Batch removal
-        if ($this->request->hasPost('toDelete')) {
-            $ids = array_keys($this->request->getPost('toDelete'));
+        if ($this->request->hasPost('batch')) {
+            $ids = array_keys($this->request->getPost('batch'));
 
             $service->deleteByIds($ids);
             $this->flashBag->set('success', 'Selected elements have been removed successfully');
