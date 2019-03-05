47e23203f28cd3fe867510e88075b1b7fd2672b1
dave-yang <daworld.ny@gmail.com>
2019-03-05 23:24:31 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name'  => 'Faq',
- 'caption'  => 'Faq',
- 'route' => 'Faq:Admin:Faq@gridAction',
- 'order' => 1,
- 'description' => 'FAQ module allows you to manage set of FAQs on your site',
+    'name'  => 'Faq',
+    'description' => 'FAQ module allows you to manage set of FAQs on your site',
+    'menu' => array(
+        'name' => 'Faq',
+        'icon' => 'fas fa-question',
+        'items' => array(
+            array(
+                'route' => 'Faq:Admin:Faq@gridAction',
+                'name' => 'View all FAQs'
+            ),
+            array(
+                'route' => 'Faq:Admin:Faq@addAction',
+                'name' => 'Add new FAQ'
+            ),
+            array(
+                'route' => 'Faq:Admin:Category@addAction',
+                'name' => 'Add a category'
+            )
+        )
+    )
 );
