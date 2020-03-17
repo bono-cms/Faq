3f74cf374e31966dc728973ecc1b3a663fc34111
dave-yang <daworld.ny@gmail.com>
2020-03-17 17:35:59 +0500

    Added SiteService

---
+++ Service/SiteService.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Faq\Service;
+
+final class SiteService
+{
+    /**
+     * Faq service
+     * 
+     * @var \Faq\Service\FaqManager
+     */
+    private $faqManager;
+
+    /**
+     * State initialization
+     * 
+     * @param \Faq\Service\FaqManager $faqManager
+     * @return void
+     */
+    public function __construct(FaqManager $faqManager)
+    {
+        $this->faqManager = $faqManager;
+    }
+
+    /**
+     * Get all FAQs
+     * 
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->faqManager->fetchAllPublished();
+    }
+}
